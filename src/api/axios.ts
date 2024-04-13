06f5fa9c21bd48429d51d81ffd3a29d2d15972ff
AlfianChabib <alfianchabib109@gmail.com>
2024-04-13 13:27:37 +0700

    commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 // const aladhanApiUrl = import.meta.env.VITE_ALADHAN_API_BASE_URL;
 
 export const aladhan = axios.create({
-  baseURL: "http://api.aladhan.com/v1/",
+  baseURL: "https://api.aladhan.com/v1/",
   headers: { "Content-Type": "application/json" },
 });
